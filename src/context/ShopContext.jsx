553da67b23d8f30b6f9d0554736e627e43d595f9
Arya Sankar <aryasankar16@gmail.com>
2026-01-31 13:13:30 +0530

    3rd Day

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -9,6 +10,58 @@ const ShopContextProvider = (props)=>{
     const delivery_fee = 10;
     const [search,setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async(ItemId, size) =>{
+
+        if(!size){
+            toast.error("Select Product Size");
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+        if(cartData[ItemId]){
+            if(cartData[ItemId][size]){
+                cartData[ItemId][size] += 1;
+            }
+
+            else{
+                cartData[ItemId][size] = 1;
+            }
+        }
+        else{
+            cartData[ItemId] = {};
+            cartData[ItemId][size] = 1;
+        }
+
+        setCartItems(cartData);
+
+    }
+
+    const getCartCount = ()=>{
+        let totalCount = 0;
+
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems[items][item] >0){
+                        totalCount += cartItems[items][item];
+                    }
+                    
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    useEffect(()=>{
+
+        console.log(cartItems);
+
+    },[cartItems])
+
     const value = {
         products,
         currency,
@@ -17,6 +70,9 @@ const ShopContextProvider = (props)=>{
         search,
         setSearch,
         setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount,
     }
 
     return (
